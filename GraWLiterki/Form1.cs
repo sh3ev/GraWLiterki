722e55b15f7b65da011f628c52bd9871c6c59265
sh3ev <sheevron@gmail.com>
2015-09-05 17:55:27 +0200

    Stats class, timer, and statusStrip controller added.

--- GraWLiterki/Form1.cs
+++ GraWLiterki/Form1.cs
@@ -12,9 +12,23 @@ namespace GraWLiterki
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        Stats stats = new Stats();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Dodaje losową literę do kontrolki  ListBox
+            listBox1.Items.Add((Keys) random.Next(65, 90));
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Koniec gry");
+                timer1.Stop();
+            }
         }
     }
 }
